05d4b4498514fae0f55c45da23cd200d3d246351
Rajitha <rajithat1998@gmail.com>
2023-04-22 20:44:48 +0530

    update

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,23 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
 
 contract TodoList {
   uint public taskCount = 0;
-}
+
+  struct Task{
+     uint id;
+     string content;
+     bool compiled;
+  }
+  mapping(uint =>Task)public tasks;
+
+   constructor()public{
+         createTask("Create TodoList");
+   }
+
+  function createTask(string memory _content) public{
+     taskCount++;
+     tasks[taskCount]=Task(taskCount, _content,false);
+  }
+
+}task=await todoList.tasks(1)
